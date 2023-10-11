d7adaf1b5a18e61dd0e7f0160cc702bd324736b2
Walid <w.cawasa@gmail.com>
2023-10-11 20:48:11 +0800

    .vscodoe spaces

--- components/login_logout - button/Login_Logout.js
+++ components/login_logout - button/Login_Logout.js
@@ -7,16 +7,16 @@ import { signOut } from "next-auth/react"
 import { signIn } from "next-auth/react"
 
 const Login_Logout = () => {
-  const { data: session } = useSession()
+	const { data: session } = useSession()
 
-  const handleEvent = () => {
-    session !== null
-      ? signOut()
-      : signIn()
-  }
-  return (
-    <button className='login-logout' onClick={(handleEvent)}> {session?.user.name ? 'Logout' : 'Login'}</button>
-  )
+	const handleEvent = () => {
+		session !== null
+			? signOut()
+			: signIn()
+	}
+	return (
+		<button className='login-logout' onClick={(handleEvent)}> {session?.user.name ? 'Logout' : 'Login'}</button>
+	)
 }
 
 export default Login_Logout
