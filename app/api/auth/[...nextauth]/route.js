60fe04db6d85f1b1995a018ec4807c3abe63cd1a
Walid <w.cawasa@gmail.com>
2023-10-11 20:45:47 +0800

    indentation

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,61 +8,61 @@ const prisma = new PrismaClient();
 const secret = process.env.NEXTAUTH_SECRET
 
 export const authOptions = {
-  session: {
-    strategy: 'jwt'
+	session: {
+		strategy: 'jwt'
 
-  },
-  jwt: {
-    secret: secret, // Access the secret from environment variables
-  },
-  secret: secret,
-  providers: [
-    CredentialsProvider({
-      name: 'Email and Password',
-      credentials: {
-        email: {
-          label: 'Email',
-          type: 'email',
-          placeHolder: 'hello@example.com'
-        },
-        password: { label: 'Password', type: 'password' }
-      },
-      async authorize(credentials) {
-        //handle Auth
-        if (!credentials.email || !credentials.password) {
-          return null
-        }
+	},
+	jwt: {
+		secret: secret, // Access the secret from environment variables
+	},
+	secret: secret,
+	providers: [
+		CredentialsProvider({
+			name: 'Email and Password',
+			credentials: {
+				email: {
+					label: 'Email',
+					type: 'email',
+					placeHolder: 'hello@example.com'
+				},
+				password: { label: 'Password', type: 'password' }
+			},
+			async authorize(credentials) {
+				//handle Auth
+				if (!credentials.email || !credentials.password) {
+					return null
+				}
 
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email
-          }
-        })
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials.email
+					}
+				})
 
-        if (!user) {
-          return null
-        }
-        const isPasswordValid = await compare(
-          credentials.password,
-          user.password
-        )
-        if (!isPasswordValid) {
-          return null
-        }
-        return {
-          id: user.id + '',
-          email: user.email,
-          name: user.name
-        }
-      }
-    })
-  ],
-  theme: {
-    colorScheme: "dark", // "auto" | "dark" | "light"
-    brandColor: "", // Hex color code
-    logo: "https://github.com/koribot/nextjs-images/raw/main/logo-white-no-background.png?raw=true", // Absolute URL to image
-    buttonText: "" // Hex color code
-  }
+				if (!user) {
+					return null
+				}
+				const isPasswordValid = await compare(
+					credentials.password,
+					user.password
+				)
+				if (!isPasswordValid) {
+					return null
+				}
+				return {
+					id: user.id + '',
+					email: user.email,
+					name: user.name
+				}
+			}
+		})
+	],
+	theme: {
+		colorScheme: "dark", // "auto" | "dark" | "light"
+		brandColor: "", // Hex color code
+		logo: "https://github.com/koribot/nextjs-images/raw/main/logo-white-no-background.png?raw=true", // Absolute URL to image
+		buttonText: "" // Hex color code
+	}
 }
 
 export const handler = NextAuth(authOptions);
